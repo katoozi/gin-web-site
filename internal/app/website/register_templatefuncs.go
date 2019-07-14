55a5af272fba70c684fb5a2f5cae72247fd4e1f2
mohammad katoozi <k2527806@gmail.com>
2019-07-14 11:02:13 +0430

    rename templatetags package to templatefuncs

--- internal/app/website/register_templatefuncs.go
+++ internal/app/website/register_templatefuncs.go
@@ -3,16 +3,15 @@ package website
 import (
 	"html/template"
 
-	"github.com/katoozi/gin-web-site/pkg/templatetags"
-
 	"github.com/gin-gonic/gin"
+	"github.com/katoozi/gin-web-site/pkg/templatefuncs"
 )
 
 // RegisterTemplateFuncs Register all custom template funcs.
 // call it before load html templates
 func RegisterTemplateFuncs(engine *gin.Engine) {
 	engine.SetFuncMap(template.FuncMap{
-		"formatAsDate": templatetags.FormatAsDate,
-		"intComma":     templatetags.IntComma,
+		"formatAsDate": templatefuncs.FormatAsDate,
+		"intComma":     templatefuncs.IntComma,
 	})
 }
--- pkg/templatefuncs/datetime.go
+++ pkg/templatefuncs/datetime.go
@@ -1,4 +1,4 @@
-package templatetags
+package templatefuncs
 
 import (
 	"fmt"
