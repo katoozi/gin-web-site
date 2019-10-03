7e8dc18c3ecc7c5b1670ee4e41fa1c95fb7af59d
mohammad katoozi <k2527806@gmail.com>
2019-10-03 19:39:27 +0330

    export all redux codes in redux/index.js

--- project/web/src/redux/index.js
+++ project/web/src/redux/index.js
@@ -6,3 +6,6 @@ const rootReducer = combineReducers(reducers);
 const store = createStore(rootReducer);
 
 export default store;
+export * from "./actions";
+export * from "./actionTypes";
+export * from "./reducers";
