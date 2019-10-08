16879dffca3655a11fcc896e3f8dbad1635c9230
mohammad katoozi <k2527806@gmail.com>
2019-10-08 21:28:48 +0330

    make the MsgException more dynamic(it have action type attribute now)

--- project/web/src/redux/sagas.js
+++ project/web/src/redux/sagas.js
@@ -1,23 +1,17 @@
 import { call, put, takeEvery } from "redux-saga/effects";
-import { sendMsg, MsgException } from "../websocket";
-import {
-  SEND_NOTIFICATION_REQUESTED,
-  RECEIVE_NOTIFICATION,
-  SEND_NOTIFICATION_FAILED
-} from "./";
+import { sendMsg } from "../websocket";
+import { SEND_NOTIFICATION_REQUESTED, RECEIVE_NOTIFICATION } from "./";
 
 function* sendMessage(action) {
   try {
     const msg = yield call(sendMsg, action.text.text);
     yield put({ type: RECEIVE_NOTIFICATION, text: msg });
   } catch (e) {
-    if (e instanceof MsgException) {
-      yield put({
-        type: SEND_NOTIFICATION_FAILED,
-        text: e.message,
-        name: e.name
-      });
-    }
+    yield put({
+      type: e.type,
+      text: e.message,
+      name: e.name
+    });
   }
 }
 
--- project/web/src/websocket/exception.js
+++ project/web/src/websocket/exception.js
@@ -1,4 +1,7 @@
+import { SEND_NOTIFICATION_FAILED } from "../redux";
+
 export default function MsgException(message) {
   this.message = message;
   this.name = "MsgException";
+  this.type = SEND_NOTIFICATION_FAILED;
 }
